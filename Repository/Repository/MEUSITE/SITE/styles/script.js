dcca969a066395e22843b9474f9886d6ca1f5a0c
Pedrosf33 <139303646+Pedrosf33@users.noreply.github.com>
2024-03-13 10:29:05 -0300

    Update script.js

--- Repository/Repository/MEUSITE/SITE/styles/script.js
+++ Repository/Repository/MEUSITE/SITE/styles/script.js
@@ -1,1 +1,11 @@
-console.log("Digite seu email!");
+function login(){
+    var email = document.getElementById("input").value;
+
+    if(!email){
+     console.log("digite seu email")
+return;
+    }
+
+    window.location.href='../login-page/login.html'
+
+}
