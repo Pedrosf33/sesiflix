8c0ebdac5a292cefe4c030a73e328e80164acc85
Pedrosf33 <139303646+Pedrosf33@users.noreply.github.com>
2024-03-15 07:48:27 -0300

    Add files via upload

---
+++ sesiflix/Repository/Repository/MEUSITE/SITE/login-page/script.js
@@ -0,0 +1,11 @@
+function login(){
+    var email = document.getElementById("input").value;
+
+    if(!email){
+     console.log("digite seu email")
+return;
+    }
+
+    window.location.href='../Opçõesdfilmes/page.html'
+
+}
